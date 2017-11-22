13aff8512e2596ed6c23e81ff0da2ea30a3650a4
guqin <gq0101@gmail.com>
2017-11-22 15:34:27 +0800

    使用Standard格式化代码

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,53 +1,53 @@
-var Chat = function(socket) {
-  this.socket = socket;
-};
+var Chat = function (socket) {
+  this.socket = socket
+}
 
-Chat.prototype.sendMessage = function(room, text) {
+Chat.prototype.sendMessage = function (room, text) {
   var message = {
     room: room,
     text: text
-  };
-  this.socket.emit('message', message);
-};
+  }
+  this.socket.emit('message', message)
+}
 
-Chat.prototype.changeRoom = function(room) {
+Chat.prototype.changeRoom = function (room) {
   this.socket.emit('join', {
     newRoom: room
-  });
-};
+  })
+}
 
-Chat.prototype.processCommand = function(command) {
-  var words = command.split(' ');
-  var command = words[0]
+Chat.prototype.processCommand = function (command) {
+  var words = command.split(' ')
+  command = words[0]
                 .substring(1, words[0].length)
-                .toLowerCase();
-  var message = false;
+                .toLowerCase()
+  var message = false
 
-  switch(command) {
+  switch (command) {
     case 'join':
-      words.shift();
-      var room = words.join(' ');
-      this.changeRoom(room);
-      break;
+      words.shift()
+      var room = words.join(' ')
+      this.changeRoom(room)
+      break
     case 'nick':
-      words.shift();
-      var name = words.join(' ');
-      this.socket.emit('nameAttempt', name);
-      break;
+      words.shift()
+      var name = words.join(' ')
+      this.socket.emit('nameAttempt', name)
+      break
     case 'dice':
-      words.shift();
-      var max = words.join('');
+      words.shift()
+      var max = words.join('')
       // 使用socketio发送消息
-      this.socket.emit('dice', max);
+      this.socket.emit('dice', max)
       // 不使用socketio发送消息
       // var msg = {name: "dice", args: [max]};
       // var msgStr = JSON.stringify(msg);
       // this.socket.send(msgStr);
-      break;
+      break
     default:
-      message = 'Unrecognized command.';
-      break;
+      message = 'Unrecognized command.'
+      break
   };
 
-  return message;
-};
+  return message
+}
--- public/js/chat_ui.js
+++ public/js/chat_ui.js
@@ -1,84 +1,84 @@
-function divEscapedContentElement(message) {
-  return $('<div class="EscapedContentElement"></div>').text(message);
+function divEscapedContentElement (message) {
+  return $('<div class="EscapedContentElement"></div>').text(message)
 }
 
-function divSystemContentElement(message) {
-  return $('<div class="SystemContentElement"></div>').html('<i>' + message + '</i>');
+function divSystemContentElement (message) {
+  return $('<div class="SystemContentElement"></div>').html('<i>' + message + '</i>')
 }
 
-function processUserInput(chatApp, socket) {
-  var message = $('#send-message').val();
-  var systemMessage;
+function processUserInput (chatApp, socket) {
+  var message = $('#send-message').val()
+  var systemMessage
 
-  if (message.charAt(0) == '/') {
-    systemMessage = chatApp.processCommand(message);
+  if (message.charAt(0) === '/') {
+    systemMessage = chatApp.processCommand(message)
     if (systemMessage) {
-      $('#messages').append(divSystemContentElement(systemMessage));
+      $('#messages').append(divSystemContentElement(systemMessage))
     }
   } else {
-    chatApp.sendMessage($('#room').text(), message);
-    $('#messages').append(divEscapedContentElement(message));
-    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+    chatApp.sendMessage($('#room').text(), message)
+    $('#messages').append(divEscapedContentElement(message))
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'))
   }
 
-  $('#send-message').val('');
+  $('#send-message').val('')
 }
 
-var socket = io.connect();
+var socket = io.connect()
 
-$(document).ready(function() {
-  var chatApp = new Chat(socket);
+$(document).ready(function () {
+  var chatApp = new Chat(socket)
 
-  socket.on('nameResult', function(result) {
-    var message;
+  socket.on('nameResult', function (result) {
+    var message
 
     if (result.success) {
-      message = 'You are now known as ' + result.name + '.';
+      message = 'You are now known as ' + result.name + '.'
     } else {
-      message = result.message;
+      message = result.message
     }
-    $('#messages').append(divSystemContentElement(message));
-  });
+    $('#messages').append(divSystemContentElement(message))
+  })
 
-  socket.on('joinResult', function(result) {
-    $('#room').text(result.room);
-    $('#messages').append(divSystemContentElement('Room changed.'));
-  });
+  socket.on('joinResult', function (result) {
+    $('#room').text(result.room)
+    $('#messages').append(divSystemContentElement('Room changed.'))
+  })
 
   socket.on('message', function (message) {
-    var newElement = $('<div></div>').text(message.text);
-    $('#messages').append(newElement);
-  });
+    var newElement = $('<div></div>').text(message.text)
+    $('#messages').append(newElement)
+  })
 
-  socket.on('rooms', function(rooms) {
-    $('#room-list').empty();
+  socket.on('rooms', function (rooms) {
+    $('#room-list').empty()
 
-    for(var room in rooms) {
-      room = room.substring(1, room.length);
-      if (room != '') {
-        $('#room-list').append(divEscapedContentElement(room));
+    for (var room in rooms) {
+      room = room.substring(1, room.length)
+      if (room !== '') {
+        $('#room-list').append(divEscapedContentElement(room))
       }
     }
 
-    $('#room-list div').click(function() {
-      chatApp.processCommand('/join ' + $(this).text());
-      $('#send-message').focus();
-    });
-  });
-  
+    $('#room-list div').click(function () {
+      chatApp.processCommand('/join ' + $(this).text())
+      $('#send-message').focus()
+    })
+  })
+
   // setInterval(function() {
   //   socket.emit('rooms');
   // }, 1000);
-  
-  socket.on('dice', function(message){
-    var newElement = $('<div></div>').text(message);
-    $('#messages').append(newElement);
-  });
-  
-  $('#send-message').focus();
 
-  $('#send-form').submit(function() {
-    processUserInput(chatApp, socket);
-    return false;
-  });
-});
+  socket.on('dice', function (message) {
+    var newElement = $('<div></div>').text(message)
+    $('#messages').append(newElement)
+  })
+
+  $('#send-message').focus()
+
+  $('#send-form').submit(function () {
+    processUserInput(chatApp, socket)
+    return false
+  })
+})
